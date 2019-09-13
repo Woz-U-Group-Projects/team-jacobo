2694c6affda3bfb6ef8ac7bffebe902ab7b9be94
ketchuponpizza <alexjacobo@icloud.com>
2019-09-13 14:29:38 -0700

    pushing code of recipe website

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import Project from "./Project";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      hello 
-    </div>
-  );
-}
+const App = () => {
+
+  const APP_ID = "df5d33a6";
+  const APP_KEY = "dc7aeb7bdf3b4c1e7a211c5411cda9ad";
+
+
+    useEffect(  () =>{
+      getRecipes();
+    }, []);
+
+
+
+  const getRecipes = async () => {
+    const response = await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}');
+    const data = await response.json();
+    console.log(data); 
+  }
+
+    return (
+      <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text" />
+          <button className="search-button" type="submit">Search</button>
+        </form>  
+      </div>
+    );
+};
 
 export default App;
