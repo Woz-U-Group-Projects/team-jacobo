194d248a6b31b7078737e8258fb40df4e9d8a6d7
ketchuponpizza <alexjacobo@icloud.com>
2019-09-16 03:26:46 -0700

    getting the button to work with the remove all button

--- client-react/public/scripts/app.js
+++ client-react/public/scripts/app.js
@@ -18,59 +18,80 @@ var onFormSubmit = function onFormSubmit(e) {
         //reference this string
         todoApp.options.push(option); //push to array 
         e.target.elements.option.value = '';
+        render();
     }
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        todoApp.title,
-        ' '
-    ),
-    todoApp.subtitle && React.createElement(
-        'p',
-        null,
-        todoApp.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        todoApp.options.length > 0 ? 'Sample Text' : 'None'
-    ),
-    React.createElement(
-        'p',
-        null,
-        todoApp.options.length
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    ),
-    React.createElement(
-        'form',
-        { onSubmit: onFormSubmit },
-        React.createElement('input', { type: 'text', name: 'option' }),
-        React.createElement(
-            'button',
-            null,
-            'Add Options'
-        )
-    )
-) //fixed error by placing <div> tags
-; //cleaned up for easier to read with indentations
+// create a Remove All button with onClick handler 
+// onclick -> wipe the array >rerenders     
+
+
+var onRemoveAll = function onRemoveAll() {
+    todoApp.options = []; //clears out all the items
+    render();
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+//create a function, set it up to take zero arguments
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            todoApp.title,
+            ' '
+        ),
+        todoApp.subtitle && React.createElement(
+            'p',
+            null,
+            todoApp.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            todoApp.options.length > 0 ? 'Sample Text' : 'None'
+        ),
+        React.createElement(
+            'p',
+            null,
+            todoApp.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Options'
+            )
+        )
+    ) //fixed error by placing <div> tags
+    ; //cleaned up for easier to read with indentations
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- client-react/src/App.js
+++ client-react/src/App.js
@@ -16,26 +16,43 @@ const onFormSubmit = (e) => {
  if (option) { //reference this string
      todoApp.options.push(option); //push to array 
      e.target.elements.option.value = '';
+     render();
  }
 };
 
+// create a Remove All button with onClick handler 
+// onclick -> wipe the array >rerenders     
 
-const template = (
-    <div>
-        <h1>{todoApp.title} </h1>
-        {todoApp.subtitle && <p>{todoApp.subtitle}</p>}
-        <p>{todoApp.options.length > 0 ? 'Sample Text' : 'None'}</p>
-        <p>{todoApp.options.length}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-        <form onSubmit={onFormSubmit}>       
-        <input type="text" name="option"/>
-        <button>Add Options</button>
-        </form>
-    </div> //fixed error by placing <div> tags
-); //cleaned up for easier to read with indentations
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+const onRemoveAll = () => {
+    todoApp.options = []; //clears out all the items
+    render();
+};
+
+const appRoot = document.getElementById('app');
+
+
+//create a function, set it up to take zero arguments
+const render = () => {
+    const template = (
+        <div>
+            <h1>{todoApp.title} </h1>
+            {todoApp.subtitle && <p>{todoApp.subtitle}</p>}
+            <p>{todoApp.options.length > 0 ? 'Sample Text' : 'None'}</p>
+            <p>{todoApp.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add Options</button>
+            </form>
+        </div> //fixed error by placing <div> tags
+    ); //cleaned up for easier to read with indentations
+
+    ReactDOM.render(template, appRoot);
+};
+
+render(); 
