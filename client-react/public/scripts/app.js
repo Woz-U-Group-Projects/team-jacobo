f9126d9a8eb9a6bd391b6a7c2cc3c9f4a33b293e
ketchuponpizza <alexjacobo@icloud.com>
2019-09-16 03:12:43 -0700

    renders but not fully because it was dumping search strings into URL

--- client-react/public/scripts/app.js
+++ client-react/public/scripts/app.js
@@ -4,7 +4,21 @@ console.log('App.js is running!');
 
 var todoApp = {
     title: 'To do App!',
-    subtitle: 'Your to do stuff!'
+    subtitle: 'Your to do stuff!',
+    options: []
+};
+
+//set up onFormSubmit arrow function
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value; // points to the element that the event started on
+
+    if (option) {
+        //reference this string
+        todoApp.options.push(option); //push to array 
+        e.target.elements.option.value = '';
+    }
 };
 
 var template = React.createElement(
@@ -16,10 +30,20 @@ var template = React.createElement(
         todoApp.title,
         ' '
     ),
-    React.createElement(
+    todoApp.subtitle && React.createElement(
         'p',
         null,
         todoApp.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        todoApp.options.length > 0 ? 'Sample Text' : 'None'
+    ),
+    React.createElement(
+        'p',
+        null,
+        todoApp.options.length
     ),
     React.createElement(
         'ol',
@@ -34,11 +58,19 @@ var template = React.createElement(
             null,
             'Item two'
         )
+    ),
+    React.createElement(
+        'form',
+        { onSubmit: onFormSubmit },
+        React.createElement('input', { type: 'text', name: 'option' }),
+        React.createElement(
+            'button',
+            null,
+            'Add Options'
+        )
     )
 ) //fixed error by placing <div> tags
 ; //cleaned up for easier to read with indentations
 
-
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,24 +1,41 @@
 console.log('App.js is running!');
 
 
-var todoApp = {
+const todoApp = {
     title: 'To do App!',
     subtitle: 'Your to do stuff!',
+    options: []
 };
 
-var template = ( 
+//set up onFormSubmit arrow function
+const onFormSubmit = (e) => {
+ e.preventDefault();
+
+ const option = e.target.elements.option.value;   // points to the element that the event started on
+
+ if (option) { //reference this string
+     todoApp.options.push(option); //push to array 
+     e.target.elements.option.value = '';
+ }
+};
+
+
+const template = (
     <div>
         <h1>{todoApp.title} </h1>
-        <p>{todoApp.subtitle}</p>
+        {todoApp.subtitle && <p>{todoApp.subtitle}</p>}
+        <p>{todoApp.options.length > 0 ? 'Sample Text' : 'None'}</p>
+        <p>{todoApp.options.length}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
         </ol>
+        <form onSubmit={onFormSubmit}>       
+        <input type="text" name="option"/>
+        <button>Add Options</button>
+        </form>
     </div> //fixed error by placing <div> tags
 ); //cleaned up for easier to read with indentations
 
-
-
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
