a2a854cb7836a9327ab653e9b77b28a73e492384
ketchuponpizza <alexjacobo@icloud.com>
2019-09-15 22:51:34 -0700

    cleaned up code and forming a rough draft of the to do list app

--- client-react/public/scripts/app.js
+++ client-react/public/scripts/app.js
@@ -3,19 +3,35 @@
 console.log('App.js is running!');
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'To-Do App '
-  ),
-  React.createElement(
-    'p',
-    null,
-    ' Testing some info'
-  )
-); //fixed error by placing <div> tags
+    React.createElement(
+        'h1',
+        null,
+        'To-Do App '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Testing some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+) //fixed error by placing <div> tags
+; //cleaned up for easier to read with indentations
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,6 +1,16 @@
 console.log('App.js is running!');
 
-var template = <div><h1>To-Do App </h1><p> Testing some info</p></div>; //fixed error by placing <div> tags
+var template = ( 
+    <div>
+        <h1>To-Do App </h1>
+        <p> Testing some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div> //fixed error by placing <div> tags
+); //cleaned up for easier to read with indentations
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
